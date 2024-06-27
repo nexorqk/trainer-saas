449b063016a2d291b461c686c7076a5865022ede
nexorqk <pkms2a10@gmail.com>
2024-06-27 17:35:50 +0300

    feat: add tailwind, signup tabs, trainer signup

---
+++ src/constants.ts
@@ -0,0 +1,48 @@
+type SelectType = { value: string; label: string }[];
+
+export const sportTypesArr: SelectType = [
+  {
+    value: "wrestling",
+    label: "Борьба",
+  },
+  {
+    value: "gymnastics",
+    label: "Гимнастика",
+  },
+  {
+    value: "fightings",
+    label: "Единоборства",
+  },
+  {
+    value: "gaming",
+    label: "Игровые",
+  },
+  {
+    value: "intelligent",
+    label: "Интеллектуальные",
+  },
+  {
+    value: "cybersport",
+    label: "Киберспорт",
+  },
+  {
+    value: "athletics",
+    label: "Легкая атлетика",
+  },
+  {
+    value: "power",
+    label: "Силовые",
+  },
+  {
+    value: "technical",
+    label: "Технические",
+  },
+  {
+    value: "fitness",
+    label: "Фитнес",
+  },
+  {
+    value: "hobby",
+    label: "Хобби",
+  },
+];
---
+++ tasks.md
@@ -0,0 +1,9 @@
+# MVP App
+
+### Create front
+
+- Make deploy
+- Make ui for auth
+  - Sign Up
+
+### Create Convex backend
