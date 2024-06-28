30d27a5725aa79b0a1f7d3fbf8f169ae84d8d2e8
nexorqk <pkms2a10@gmail.com>
2024-06-28 05:12:55 +0300

    fix: default convex url import

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-const deploymentUrl =
-  import.meta.env.MODE === "development"
-    ? import.meta.env.VITE_CONVEX_URL
-    : process.env.VITE_CONVEX_URL;
-
-const convex = new ConvexReactClient(deploymentUrl);
+const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
