8181bc9302b7f70b2c8435dd9a8c34e93da49377
nexorqk <pkms2a10@gmail.com>
2024-06-28 04:50:56 +0300

    fix: hardcode link

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL);
+const convex = new ConvexReactClient(
+  "https://superb-reindeer-745.convex.cloud"
+);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
