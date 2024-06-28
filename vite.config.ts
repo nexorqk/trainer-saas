62638bec05c0a30aef57fa1957fd6579bf63a560
nexorqk <pkms2a10@gmail.com>
2024-06-28 04:26:51 +0300

    fix: vite config backend outDir

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/trainer-sass/",
   plugins: [react()],
+  build: {
+    outDir: "docs",
+  },
+  base: "/trainer-sass/",
 });
