b9dfff3c2aacaf6092a82299abfc26840a5a27e7
nexorqk <pkms2a10@gmail.com>
2024-06-28 04:13:37 +0300

    fix: logs for context url

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,10 @@ import "./index.css";
 
 const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
 
+console.log(import.meta.env.VITE_CONVEX_URL);
+
+console.log(convex);
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ConvexProvider client={convex}>
