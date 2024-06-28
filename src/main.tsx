58897adae2039618a6cb79f554a7bcaa20daa99e
nexorqk <pkms2a10@gmail.com>
2024-06-28 04:47:34 +0300

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
-
-console.log(import.meta.env.VITE_CONVEX_URL);
-
-console.log(convex);
+const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
